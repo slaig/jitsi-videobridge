6706de3081d2866166a7c6c217cc15f2a507c32f
paweldomas <pawel.domas@jitsi.org>
2016-07-26 15:46:24 -0500

    feat(Conference): add "SCTP connection ready" event
    The event will be triggered when SCTP connection has been
    established for given Endpoint.

--- src/main/java/org/jitsi/videobridge/Conference.java
+++ src/main/java/org/jitsi/videobridge/Conference.java
@@ -1482,6 +1482,12 @@ public class Conference
                  * implement it as well.
                  */
                 endpoint.sctpConnectionReady(sctpConnection);
+                // Trigger SCTP connection ready event
+                if (eventAdmin != null)
+                {
+                    eventAdmin.postEvent(
+                            EventFactory.endpointSctpConnReady(endpoint));
+                }
             }
         }
     }
