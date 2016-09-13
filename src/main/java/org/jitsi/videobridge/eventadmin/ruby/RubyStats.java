f8242d6831c4d49ffb09cd25edbfc7c37dd44ad4
Igor Shlahov <slaigv@gmail.com>
2016-09-13 18:10:11 +0300

    Store ruby stats to influx, correction2

--- src/main/java/org/jitsi/videobridge/eventadmin/ruby/RubyStats.java
+++ src/main/java/org/jitsi/videobridge/eventadmin/ruby/RubyStats.java
@@ -8,6 +8,7 @@ import org.jitsi.service.neomedia.MediaType;
 import org.jitsi.service.neomedia.stats.MediaStreamStats2;
 import org.jitsi.service.neomedia.stats.ReceiveTrackStats;
 import org.jitsi.service.neomedia.stats.SendTrackStats;
+import org.jitsi.service.neomedia.stats.TrackStats;
 
 /**
  * Store statistics to influx
@@ -20,48 +21,47 @@ public class RubyStats extends AbstractLoggingHandler {
 
     public void reportOutbound(String bridgeId, String conferenceID, String endpointID, MediaType mediaType,
                                MediaStreamStats2 stats, SendTrackStats sendStat) {
-
-        Point.Builder ptBuilder = Point.measurement("videobridge_conference_stats");
-        ptBuilder.tag("bridgeId", bridgeId);
-        ptBuilder.tag("conferenceId", conferenceID);
-        ptBuilder.tag("endpointId", endpointID);
-        ptBuilder.tag("mediaType", mediaType.name());
-        ptBuilder.tag("flowType", "outbound");
-
-        ptBuilder.tag("ssrc", String.valueOf(sendStat.getSSRC()));
-
-        ptBuilder.field("jitter", stats.getSendStats().getJitter());
-        ptBuilder.field("rtt", stats.getSendStats().getRtt());
-
-        ptBuilder.field("bytesSent", sendStat.getCurrentBytes());
-        ptBuilder.field("packetsSent", sendStat.getCurrentPackets());
-        ptBuilder.field("packetRateSent", sendStat.getPacketRate());
-        ptBuilder.field("lossRateSent", sendStat.getLossRate());
-        ptBuilder.field("bitrateSent", sendStat.getBitrate());
-
-        Point point = ptBuilder.build();
-
-        writePoint(point);
+        report(bridgeId, conferenceID, endpointID, mediaType, stats, sendStat);
     }
 
-    public void reportInbound(String bridgeId, String conferenceID, String endpointID, MediaType mediaType, MediaStreamStats2 stats, ReceiveTrackStats receiveStat) {
+    public void reportInbound(String bridgeId, String conferenceID, String endpointID, MediaType mediaType,
+                              MediaStreamStats2 stats, ReceiveTrackStats receiveStat) {
+        report(bridgeId, conferenceID, endpointID, mediaType, stats, receiveStat);
+    }
+
+    private void report(String bridgeId, String conferenceID, String endpointID, MediaType mediaType, MediaStreamStats2 stats, TrackStats trackStats) {
         Point.Builder ptBuilder = Point.measurement("videobridge_conference_stats");
         ptBuilder.tag("bridgeId", bridgeId);
         ptBuilder.tag("conferenceId", conferenceID);
         ptBuilder.tag("endpointId", endpointID);
         ptBuilder.tag("mediaType", mediaType.name());
-        ptBuilder.tag("flowType", "inbound");
 
-        ptBuilder.tag("ssrc", String.valueOf(receiveStat.getSSRC()));
+        ptBuilder.tag("ssrc", String.valueOf(trackStats.getSSRC()));
 
         ptBuilder.field("jitter", stats.getReceiveStats().getJitter());
         ptBuilder.field("rtt", stats.getReceiveStats().getRtt());
 
-        ptBuilder.field("bytesReceive", receiveStat.getCurrentBytes());
-        ptBuilder.field("packetsReceive", receiveStat.getCurrentPackets());
-        ptBuilder.field("packetRateReceive", receiveStat.getPacketRate());
-        ptBuilder.field("bitrateReceive", receiveStat.getBitrate());
-        ptBuilder.field("packetLostReceive", receiveStat.getCurrentPacketsLost());
+        ptBuilder.field("bytes", trackStats.getBytes());
+        ptBuilder.field("currentBytes", trackStats.getCurrentBytes());
+        ptBuilder.field("packets", trackStats.getPackets());
+        ptBuilder.field("currentPackets", trackStats.getCurrentPackets());
+        ptBuilder.field("packetRate", trackStats.getPacketRate());
+        ptBuilder.field("bitrate", trackStats.getBitrate());
+        ptBuilder.field("interval", trackStats.getInterval());
+
+        if (trackStats instanceof ReceiveTrackStats)
+        {
+            ptBuilder.tag("flowType", "inbound");
+
+            ptBuilder.field("packetsLost", ((ReceiveTrackStats)trackStats).getPacketsLost());
+            ptBuilder.field("currentPacketsLost", ((ReceiveTrackStats)trackStats).getCurrentPacketsLost());
+        }
+        else if (trackStats instanceof SendTrackStats)
+        {
+            ptBuilder.tag("flowType", "outbound");
+
+            ptBuilder.field("lossRateSent", ((SendTrackStats) trackStats).getLossRate());
+        }
 
         Point point = ptBuilder.build();
 
